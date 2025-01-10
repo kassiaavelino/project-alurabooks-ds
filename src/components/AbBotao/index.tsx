4ba7709452e05e4c8adb97193d15076f16e34858
Kássia Espíndula Avelino <kassia.avelino@prosperita.io>
2025-01-10 11:49:59 -0300

    update component AbBotao
    Signed-off-by: Kássia Espíndula Avelino <kassia.avelino@prosperita.io>

--- src/components/AbBotao/index.tsx
+++ src/components/AbBotao/index.tsx
@@ -1,21 +1,44 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-const BotaoEstilizado = styled.button`
-  background: #EB9B00;
+export interface AbBotaoProps {
+  text?: string;
+  type?: "primary" | "secondary";
+  onClick?: () => void
+}
+
+const BotaoEstilizado = styled.button<AbBotaoProps>`
+  background: ${props => props.type === "primary" ? "#EB9B00" : "#FFF"};
   padding: 16px 32px;
   border: 2px solid #EB9B00;
-  color: #FFF;
+  color: ${props => props.type === "primary" ? "#FFF" : "#EB9B00"};
   font-size: 20px;
   cursor: pointer;
-  &:hover {
+  ${props => props.type === "primary"
+    ? css`
+        &:hover {
+          background: #B87900;
+          border: 2px solid #B87900;
+        }
+      `
+    : css`
+        &:hover {
+          background: #FFF;
+          border: 2px solid #B87900;
+          color: #B87900;
+        }
+      `
+  };
+  /* &:hover {
     background: #B87900;
     border: 2px solid #B87900;
-  }
+  } */
 `;
 
-export const AbBotao = () => {
+export const AbBotao = ({ text, type = "primary", onClick }: AbBotaoProps) => {
   return (
-    <BotaoEstilizado>Clique aqui!</BotaoEstilizado>
+    <BotaoEstilizado onClick={onClick} type={type}>
+      {text}
+    </BotaoEstilizado>
   );
 }
--- stories/AbBotao.stories.tsx
+++ stories/AbBotao.stories.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import {  ComponentMeta, ComponentStory } from '@storybook/react'
-import { AbBotao } from '../src'
+import { AbBotao, AbBotaoProps } from '../src'
 
 export default {
   title: 'Components/AbBotao',
   component: AbBotao
 } as ComponentMeta<typeof AbBotao>
 
-const Template: ComponentStory<typeof AbBotao> = () => <AbBotao />
+const Template: ComponentStory<typeof AbBotao> = (args) => <AbBotao {...args} />
 
 export const Primario = Template.bind({})
+
+Primario.args = {
+  text: "Ab Botão Primário",
+  type: "primary",
+} as AbBotaoProps
+
+export const Secundario = Template.bind({})
+
+Secundario.args = {
+  text: "Ab Botão Secundário",
+  type: "secondary",
+} as AbBotaoProps
